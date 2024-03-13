dc6421abc59a31865a7cb18eb9f897b209407c26
MikeBilbee <cmbilbee@gmail.com>
2024-03-12 20:52:26 -0500

    0.0 welcome

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,8 @@
+/* Prints a welcome message */
+function welcome(firstName, lastName) {
+  const fullName = firstName + ' ' + lastName;
+  function displayFullName() {
+    alert('Welcome ' + fullName + '!');
+  }
+  displayFullName(fullName);
+}
