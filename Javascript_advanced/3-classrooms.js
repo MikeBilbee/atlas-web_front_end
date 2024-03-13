0bb3eeb0f46e03fc3c29235c3fec7ce9cd391e91
MikeBilbee <cmbilbee@gmail.com>
2024-03-12 21:00:51 -0500

    3.0 loop class

---
+++ Javascript_advanced/3-classrooms.js
@@ -0,0 +1,13 @@
+/* Creates a "model" of a class room */
+function createClassRoom(numberOfStudents) {
+  function studentSteat(seat) {
+    return () => seat;
+    }
+  let students = Array();
+  for (let i = 0; i < numberOfStudents; i++) {
+    students.push(studentSteat(i + 1));
+  }
+  return students;
+}
+
+const classRoom = createClassRoom(10);
