958f1bf5ab330a7872f860717a508ff3e58328d3
MikeBilbee <cmbilbee@gmail.com>
2024-03-12 21:03:30 -0500

    4.0 math

---
+++ Javascript_advanced/4-math.js
@@ -0,0 +1,13 @@
+/* A division and an addition function */
+function divideBy(firstNumber) {
+  return(secondNumber) => (secondNumber / firstNumber);
+}
+
+function addBy(firstNumber) {
+  return(secondNumber) => (firstNumber + secondNumber);
+}
+
+const addBy100 = addBy(100);
+const addBy1000 = addBy(1000);
+const divideBy10 = divideBy(10);
+const divideBy100 = divideBy(100);
