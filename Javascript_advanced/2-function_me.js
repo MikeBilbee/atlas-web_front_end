6bc43810cdefd4f65dd0c34d26e370bcc7298b5c
MikeBilbee <cmbilbee@gmail.com>
2024-03-12 20:57:45 -0500

    2.0 welcome user

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,9 @@
+/* Welcome user */
+function welcomeMessage (fullName) {
+  alert("Welcome " + fullName)
+
+}
+
+const guillaume = welcomeMessage('Guillaume');
+const alex = welcomeMessage('Alex');
+const fred = welcomeMessage('Fred');
