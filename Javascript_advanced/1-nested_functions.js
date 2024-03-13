738bc300fce04e203c9bf3238f190e7d1a5c1cff
MikeBilbee <cmbilbee@gmail.com>
2024-03-12 20:54:38 -0500

    1.0 welcome to holberton

---
+++ Javascript_advanced/1-nested_functions.js
@@ -0,0 +1,17 @@
+/* Prints a welcome message */
+const globalVariable = 'Welcome';
+
+function outer () {
+  alert(globalVariable);
+  const course = 'Holberton';
+  function inner() {
+    alert(globalVariable + ' ' + course);
+    const exclamation = '!';
+    function inception() {
+      alert(globalVariable + ' ' + course + exclamation);
+    }
+    inception();
+  }
+  inner();
+}
+outer();
